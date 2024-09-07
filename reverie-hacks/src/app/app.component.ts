e6062c7df14d1b054b98ef59dc8dadb77714469d
Sarah <yangsarah116@gmail.com>
2024-09-07 18:27:56 -0500

    test

--- reverie-hacks/src/app/app.component.ts
+++ reverie-hacks/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbar,
+    MatIcon,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
